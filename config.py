f7360e1cbe99addebe8e7fdabdd976f80dbe8180
Ahmed Javed <ahmedjaved701@gmail.com>
2023-02-02 21:18:05 +0500

    :sparkles: Added secret key and algorithm for jwt variables!

--- config.py
+++ config.py
@@ -6,10 +6,17 @@ from the pydantic library.
 
 The configuration is loaded from the .env file using pydantic BaseSettings class.
 """
+import secrets
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+
+    """
+        This class is used to store the configuration of the application.
+
+    """
+
     # The name of the application
     APP_NAME: str = "Elevatus Assignment"
 
@@ -18,6 +25,11 @@ class Settings(BaseSettings):
 
     MONGO_CONNECTION_STRING: str
     MONGO_DB_NAME: str
+
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    ALGORITHM: str = "HS256"
+
+    ACCESS_TOKEN_EXPIRE_HOURS: int = 12
 
     class Config:
         env_file = ".env"
