25d3ec7f165dbab87c96cbb0f9640493765a7c75
Ahmed Javed <ahmedjaved701@gmail.com>
2023-02-02 20:08:05 +0500

    :rocket: Added candidate crud routes and report route!

---
+++ routers/candidate.py
@@ -0,0 +1,136 @@
+"""
+    Candidate router
+
+"""
+
+import io
+from uuid import uuid4, UUID
+import bson
+import pandas as pd
+from fastapi import APIRouter, status
+from fastapi.responses import JSONResponse, StreamingResponse
+from core.schemas import CandidateSchema, CandidateReadSchema, CandidateUpdateSchema
+from core.database import candidates
+
+router = APIRouter()
+
+@router.post("/")
+async def create_candidate(candidate: CandidateSchema) -> CandidateReadSchema:
+    """
+        Create candidate
+
+        - **first_name**: Candidate first name (str)
+        - **last_name**: Candidate last name (str)
+        - **email**: Candidate email (str)
+        - **career_level**: Candidate career level (str)
+        - **job_major**: Candidate job major (str)
+        - **years_of_experience**: Candidate years of experience (int)
+        - **degree_type**: Candidate degree type (str)
+        - **skills**: Candidate skills (list)
+        - **nationality**: Candidate's nationality (str)
+        - **city**: Candidate's city (str)
+        - **salary**: Candidate's salary (int)
+        - **gender**: Candidate's gender (str)
+    """
+
+    # Check if candidate exists
+    if candidates.find_one({"email": candidate.email}):
+        return JSONResponse(
+            status_code=status.HTTP_409_CONFLICT, 
+            content={"message": "Candidate already exists"}
+            )
+
+    uuid = bson.Binary.from_uuid(uuid4())
+    document = {"uuid": uuid, **candidate.dict()}
+    candidates.insert_one(document)
+    return document
+
+
+@router.get("/generate-report/", status_code=status.HTTP_200_OK)
+async def generate_candidate_csv() -> StreamingResponse:
+    """
+        Generate csv file of all candidates!
+    """
+    # convert all candidates to csv
+    df = pd.DataFrame(list(candidates.find()))
+    stream = io.StringIO()
+    df.to_csv(stream, index=False)
+
+    response = StreamingResponse(
+        iter([stream.getvalue()]),
+        media_type="text/csv",
+        headers={
+            "Content-Disposition": "attachment; filename=candidates.csv"
+        }
+    )
+
+    return response
+
+
+@router.get("/{uuid}")
+async def get_candidate(uuid: UUID) -> CandidateReadSchema:
+    """
+        Get candidate
+
+        - **uuid**: Candidate UUID (str)
+    """
+    document = candidates.find_one({"uuid": bson.Binary.from_uuid(uuid)})
+    if document:
+        return document
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND, 
+        content={"message": "Candidate not found"}
+    )
+
+@router.put("/{uuid}")
+@router.patch("/{uuid}")
+async def update_candidate(uuid: UUID, candidate: CandidateUpdateSchema) -> CandidateReadSchema:
+    """
+        Update candidate
+
+        - **first_name**: Candidate first name (str)
+        - **last_name**: Candidate last name (str)
+        - **email**: Candidate email (str)
+        - **career_level**: Candidate career level (str)
+        - **job_major**: Candidate job major (str)
+        - **years_of_experience**: Candidate years of experience (int)
+        - **degree_type**: Candidate degree type (str)
+        - **skills**: Candidate skills (list)
+        - **nationality**: Candidate's nationality (str)
+        - **city**: Candidate's city (str)
+        - **salary**: Candidate's salary (int)
+        - **gender**: Candidate's gender (str)
+    """
+
+    document = candidates.find_one({"uuid": bson.Binary.from_uuid(uuid)})
+    if document:
+        document = candidate.dict()
+        candidates.update_one({"uuid": bson.Binary.from_uuid(uuid)}, {"$set": document})
+        return {"uuid": uuid, **document}
+    return JSONResponse(
+        status_code=status.HTTP_404_NOT_FOUND,
+        content={"message": "Candidate not found"}
+        )
+
+
+@router.delete("/{uuid}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_candidate(uuid: UUID):
+    """
+        Delete candidate
+
+        - **uuid**: Candidate UUID (str)
+    """
+    document = candidates.find_one_and_delete(filter={"uuid": bson.Binary.from_uuid(uuid)})
+    if not document:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            content={"message": "Candidate not found"}
+        )
+
+
+@router.get("/")
+async def get_candidates() -> list[CandidateReadSchema]:
+    """
+        Get candidates
+    """
+    return list(candidates.find())
--- routers/routers.py
+++ routers/routers.py
@@ -4,8 +4,9 @@ This file contains the routers for the API.
 
 from fastapi import APIRouter
 from routers import user
+from routers import candidate
 
 api_router = APIRouter()
 
 api_router.include_router(user.router, prefix="/user", tags=["user"])
-
+api_router.include_router(candidate.router, prefix="/candidate", tags=["candidate"])
