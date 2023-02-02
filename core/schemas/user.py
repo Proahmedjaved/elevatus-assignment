220dfe4fa2b9816c9414b61e079173cb6668226d
Ahmed Javed <ahmedjaved701@gmail.com>
2023-02-02 17:46:08 +0500

    :sparkles: Added pydantics models for validations!

---
+++ core/schemas/user.py
@@ -0,0 +1,32 @@
+"""
+
+"""
+
+from uuid import UUID, uuid4
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserSchema(BaseModel):
+    """
+        User pydantic model
+
+        :param uuid: User UUID
+        :param first_name: User first name
+        :param last_name: User last name
+        :param email: User email
+    """
+    first_name: str = Field(..., example="John")
+    last_name: str = Field(..., example="Doe")
+    email: EmailStr = Field(..., example="john@email.com")
+
+
+class UserReadSchema(UserSchema):
+    """
+        User pydantic model
+
+        :param uuid: User UUID
+        :param first_name: User first name
+        :param last_name: User last name
+        :param email: User email
+    """
+    uuid: UUID
