407abdbe3a2efac173c254264b52c83a6f2b0713
Ahmed Javed <ahmedjaved701@gmail.com>
2023-02-02 21:20:15 +0500

    :rocket: Added auth router!

---
+++ routers/auth.py
@@ -0,0 +1,45 @@
+"""
+    Authentication Router
+
+"""
+from passlib.hash import pbkdf2_sha256
+from fastapi import APIRouter, status, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from core.database import users
+from core.security import create_access_token
+
+
+router = APIRouter()
+
+@router.post('/login', summary="Performs authentication")
+async def log_in(request: OAuth2PasswordRequestForm = Depends()):
+    """
+        Performs authentication and returns the authentication token to keep the user
+        logged in for longer time.
+
+        Provide **Username** and **Password** to log in.
+
+    """
+    print(request.username, request.password)
+    user = users.find_one({"email": request.username.lower()})
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid credentials"
+        )
+    if not pbkdf2_sha256.verify(request.password, user["password"]):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Incorrect password"
+        )
+
+    access_token = create_access_token(
+        data={
+            "sub": user["email"],
+            "uuid": user["uuid"].hex(),
+        },
+        token_type="access")
+
+    return {"access_token": access_token, "token_type": "bearer"}
--- routers/routers.py
+++ routers/routers.py
@@ -5,8 +5,10 @@ This file contains the routers for the API.
 from fastapi import APIRouter
 from routers import user
 from routers import candidate
+from routers import auth
 
 api_router = APIRouter()
 
+api_router.include_router(auth.router, tags=["auth"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(candidate.router, prefix="/candidate", tags=["candidate"])
